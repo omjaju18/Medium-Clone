b3d9a4a195d55d2ccc08c85a39939960a4bf7f64
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-09-15 19:22:16 +0530

    Fetching Data

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,21 +1,35 @@
 import ArticleMain from "@/components/Post/ArticleMain";
 import ReadersNav from "@/components/Post/ReadersNav";
 import Recommendations from "@/components/Post/Recommendations";
+import { MediumContext } from "@/context/MediumContext";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useContext, useEffect } from "react";
 
 const styles = {
   content: `flex`,
 };
 const Post = () => {
+  const { posts, users } = useContext(MediumContext);
+  const router = useRouter();
+  const [post, setPost] = useState();
+  const [author, setAuthor] = useState();
+
+  useEffect(() => {
+    setPost(posts.find((post) => post.id === router.query.slug));
+    
+    setAuthor(users.find((user) => user.id === post?.data?.author));
+  }, [post]);
+
   return (
     <>
       <Head>
-        <title>Medium | Clever Programmer</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Medium | OJ</title>
+        <link rel="icon" href="/static/logo.png" />
       </Head>
       <main className={styles.content}>
         <ReadersNav />
-        <ArticleMain />
+        <ArticleMain post={post} author={author} />
         <Recommendations />
       </main>
     </>
