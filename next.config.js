70df313888ce7efdfdd5268d465ec8acd839ee02
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2023-09-15 18:47:10 +0530

    Updated: Links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "cdn.hashnode.com",
+      "www.google.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
